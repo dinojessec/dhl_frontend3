5b94f0e6e08aa4b7d21b948f0140ef914e0f1b4b
Jesse Dino <jesse.dino.z3@gmail.com>
2019-04-13 17:52:08 +0800

    added user component

---
+++ src/views/User.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <user-component></user-component>
+    </div>
+  </div>
+</template>
+
+<script>
+import UserComponent from "../components/user/user";
+export default {
+  name: "User",
+  components: {
+    UserComponent
+  }
+};
+</script>
+
+<style scoped>
+</style>
