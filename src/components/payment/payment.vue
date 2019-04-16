170c3e4f9e0e3f78200921b393547109e5aec223
Jesse Dino <jesse.dino.z3@gmail.com>
2019-04-16 20:03:18 +0800

    created component for payment

---
+++ src/components/payment/payment.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <p>payment</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Payment"
+};
+</script>
+
+<style scoped>
+</style>
