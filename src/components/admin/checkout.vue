270cfd43e32da5219777f70602723df20821c2cc
Jesse Dino <jesse.dino.z3@gmail.com>
2019-04-18 14:37:41 +0800

    checkout get request

--- src/components/admin/checkout.vue
+++ src/components/admin/checkout.vue
@@ -4,16 +4,28 @@
     <div
       class="border rounded p-4"
       id="forPrint"
+      v-for="student in checkoutStudent"
+      :key="student.userID"
     >
       <div class="row m-2">
-        <div class="col"><label>Fullname</label></div>
-        <div class="col"><label>Date Enrolled</label></div>
+        <div class="col-8"><label class="text-muted">Fullname</label>
+          <h3>{{ student.firstName | capitalize }} {{ student.middleName | capitalize }} {{ student.lastName | capitalize }}</h3>
+        </div>
+        <div class="col-4"><label class="text-muted">Date</label>
+          <h4>{{ student.checkoutDate }}</h4>
+        </div>
       </div>
       <hr>
       <div class="row m-2">
-        <div class="col"><label>Strand</label></div>
-        <div class="col"><label>Grade Level</label></div>
-        <div class="col"><label>Section</label></div>
+        <div class="col-6"><label class="text-muted">Strand</label>
+          <h5>{{ student.strandName }}</h5>
+        </div>
+        <div class="col"><label class="text-muted">Grade Level</label>
+          <h5>{{ student.gradeLevel }}</h5>
+        </div>
+        <div class="col"><label class="text-muted">Strand</label>
+          <h5>Section</h5>
+        </div>
       </div>
       <hr>
       <div class="row">
@@ -49,6 +61,7 @@
         </div>
       </div>
     </div>
+
     <button
       class="btn btn-primary m-3"
       @click="print('forPrint')"
@@ -57,8 +70,37 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "checkout",
+  data() {
+    return {
+      checkoutStudent: []
+    };
+  },
+
+  created() {
+    const token = localStorage.getItem("token");
+    const userID = this.$route.params.userID;
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json"
+      }
+    };
+    axios
+      .get(`http://localhost:3000/api/v1/admin/checkout/${userID}`, config)
+      .then(val => {
+        const student = val.data;
+        console.log(student);
+        this.checkoutStudent = student;
+      })
+      .catch(err => {
+        throw err;
+      });
+  },
+
   methods: {
     print(forPrint) {
       const contents = document.getElementById(forPrint).innerHTML;
@@ -70,6 +112,14 @@ export default {
       window.print();
 
       document.body.innerHTML = originalContents;
+    }
+  },
+
+  filters: {
+    capitalize(value) {
+      if (!value) return "";
+      value = value.toString();
+      return value.charAt(0).toUpperCase() + value.slice(1);
     }
   }
 };
