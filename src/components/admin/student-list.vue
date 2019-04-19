f627eb4346453b17894d35082293f6f43ae982c1
Jesse Dino <jesse.dino.z3@gmail.com>
2019-04-19 18:33:20 +0800

    fixed v-for on tr tags

--- src/components/admin/student-list.vue
+++ src/components/admin/student-list.vue
@@ -1,6 +1,16 @@
 <template>
 
   <div class="container">
+    <div class="row mb-3">
+      <div class="col-4">
+        <input
+          type="text"
+          class="form-control"
+          v-model="filter"
+          placeholder="Filter Search.."
+        >
+      </div>
+    </div>
 
     <div class="table-responsive">
       <table class="table table-hover">
@@ -18,35 +28,36 @@
           </tr>
         </thead>
 
-        <template v-for="(student, index) in allStudents">
-          <tbody :key="student.studentID">
-            <tr>
-              <th scope="row">{{ index }}</th>
-              <td>
-                <router-link :to="{ path: `/profile/${student.userID}`}">{{ student.firstName | capitalize }} {{ student.middleName | capitalize }} {{ student.lastName | capitalize }}</router-link>
-              </td>
-              <td>{{ student.gradeLevel }}</td>
-              <td>{{ student.strandName }}</td>
-              <td>section</td>
-              <td>{{ student.age }}</td>
-              <td>{{ student.gender }}</td>
-              <td>
-                <div
-                  class="alert alert-danger mb-0"
-                  v-if="student.status === 'pending'"
-                >{{ student.status }}</div>
-                <router-link
-                  :to="{ path: `/admin/checkout/${student.userID}` }"
-                  v-if="student.status === 'approved'"
-                ><button
-                    type="button"
-                    class="btn btn-outline-success mb-0"
-                  >{{ student.status }}</button></router-link>
-              </td>
-              <td>{{ student.approvedBy }}</td>
-            </tr>
-          </tbody>
-        </template>
+        <tbody>
+          <tr
+            v-for="(student, index) in filteredSearch"
+            :key="student.studentID"
+          >
+            <th scope="row">{{ index }}</th>
+            <td>
+              <router-link :to="{ path: `/profile/${student.userID}`}">{{ student.firstName | capitalize }} {{ student.middleName | capitalize }} {{ student.lastName | capitalize }}</router-link>
+            </td>
+            <td>{{ student.gradeLevel }}</td>
+            <td>{{ student.strandName }}</td>
+            <td>section</td>
+            <td>{{ student.age }}</td>
+            <td>{{ student.gender }}</td>
+            <td>
+              <div
+                class="alert alert-danger mb-0"
+                v-if="student.status === 'pending'"
+              >{{ student.status }}</div>
+              <router-link
+                :to="{ path: `/admin/checkout/${student.userID}` }"
+                v-if="student.status === 'approved'"
+              ><button
+                  type="button"
+                  class="btn btn-outline-success mb-0"
+                >{{ student.status }}</button></router-link>
+            </td>
+            <td>{{ student.approvedBy }}</td>
+          </tr>
+        </tbody>
       </table>
     </div>
 
@@ -61,6 +72,8 @@ export default {
   name: "StudentList",
   data() {
     return {
+      searchResult: [],
+      filter: "",
       allStudents: []
     };
   },
@@ -75,11 +88,21 @@ export default {
     axios
       .get(`http://localhost:3000/api/v1/admin/student`, config)
       .then(response => {
-        this.allStudents = response.data.response;
+        this.searchResult = response.data.response;
+        // this.searchResult = response.data;
+        // console.log(response.data);
       })
       .catch(e => {
         console.log(e);
       });
+  },
+
+  computed: {
+    filteredSearch() {
+      return this.searchResult.filter(item => {
+        return item.firstName.match(this.filter);
+      });
+    }
   },
 
   filters: {
@@ -93,4 +116,18 @@ export default {
 </script>
 
 <style scoped>
+tbody {
+  display: block;
+  height: 600px;
+  overflow: auto;
+}
+thead,
+tbody tr {
+  display: table;
+  width: 100%;
+  table-layout: fixed;
+}
+thead {
+  width: calc(100% - 1em);
+}
 </style>
