9ee6089b483e62fe2212e9346807812ce3c2e474
Jesse Dino <jesse.dino.z3@gmail.com>
2019-04-23 19:52:03 +0800

    fixed filter result

--- src/components/admin/student-list.vue
+++ src/components/admin/student-list.vue
@@ -2,33 +2,107 @@
 
   <div class="container">
     <div class="row mb-3">
-      <div class="col-3">
-        <label>Filter By: {{ filterBy }}</label>
+      <div class="col-4">
+        <label>Search By:</label>
+        <select
+          class="custom-select"
+          v-model="searchBy"
+          @change="getAllStrand()"
+        >
+          <option
+            disabled
+            :value="null"
+            selected
+          >Choose..</option>
+          <option value="strand">Strand</option>
+          <option value="gradelevel">Grade Level</option>
+        </select>
+      </div>
+      <!-- // strand selection -->
+      <div
+        class="col-4"
+        v-if="this.searchBy === 'strand'"
+      >
+        <label>&nbsp</label>
+        <select
+          class="custom-select"
+          v-model="selectedStrand"
+        >
+          <option
+            v-for="(strand, i) in strandList"
+            :key="i"
+            :value="strand.strandID"
+          >{{ strand.strandName }}</option>
+        </select>
+      </div>
+      <!-- // grade level selection -->
+      <div
+        class="col-4"
+        v-if="this.searchBy === 'gradelevel'"
+      >
+        <label>&nbsp</label>
+        <select
+          class="custom-select"
+          v-model="selectedGrade"
+        >
+          <option value="Grade11">Grade 11</option>
+          <option value="Grade12">Grade 12</option>
+        </select>
+      </div>
+      <div class="col-2">
+        <label>&nbsp</label>
+        <input
+          v-if="this.searchBy === 'strand'"
+          type="button"
+          class="btn btn-primary form-control"
+          value="Search"
+          @click="searchStrand()"
+        >
+        <input
+          v-if="this.searchBy === 'gradelevel'"
+          type="button"
+          class="btn btn-primary form-control"
+          value="Search"
+          @click="searchGrade()"
+        >
+      </div>
+    </div>
+
+    <div class="row mb-3">
+      <div class="col-4">
+        <label>Filter By:</label>
         <select
           class="custom-select"
           v-model="filterBy"
         >
-          <option
-            value="firstName"
-            selected
-          >First Name</option>
-          <option value="lastName">Last Name</option>
-          <option value="gradeLevel">Grade Level</option>
           <option value="strandName">Strand</option>
-          <option value="section">Section</option>
-          <option value="age">Age</option>
+          <option value="firstName">First Name</option>
           <option value="gender">Gender</option>
-          <option value="status">Status</option>
-          <option value="approvedBy">Approver</option>
         </select>
       </div>
-      <div class="col-4">
+
+      <div class="col-5">
         <label>&nbsp</label>
         <input
           type="text"
           class="form-control"
           v-model="filter"
-          placeholder="Filter Search.."
+          placeholder="Filter Strand.."
+          v-if="this.filterBy === 'strandName'"
+        >
+        <input
+          type="text"
+          class="form-control"
+          v-model="filter"
+          placeholder="Filter First Name.."
+          v-if="this.filterBy === 'firstName'"
+        >
+        <input
+          type="text"
+          class="form-control"
+          v-model="filter"
+          placeholder="Filter Gender.."
+          v-if="this.filterBy === 'gender'"
         >
       </div>
     </div>
@@ -37,6 +111,7 @@
       <table class="table table-hover">
         <thead>
           <tr>
+            <th scope="col">No.</th>
             <th scope="col">LRN</th>
             <th scope="col">Name</th>
             <th scope="col">Strand</th>
@@ -45,6 +120,7 @@
             <th scope="col">School Name</th>
             <th scope="col">School Location</th>
             <th scope="col">Graduation Date</th>
+            <th scope="col">Final grade average</th>
             <th scope="col">Status</th>
             <th scope="col">Approver</th>
           </tr>
@@ -52,20 +128,21 @@
 
         <tbody>
           <tr
-            v-for="(student) in filteredSearch"
+            v-for="(student, i) in filteredSearch"
             :key="student.studentID"
           >
-            <td scope="row">{{ student.LRN }}</td>
+            <td scope="row">{{ i + 1 }}</td>
+            <td>{{ student.LRN }}</td>
             <td>
               <router-link :to="{ path: `/profile/${student.userID}`}">{{ student.firstName | capitalize }} {{ student.middleName | capitalize }} {{ student.lastName | capitalize }}</router-link>
             </td>
-            <!-- <td>{{ student.gradeLevel }}</td> -->
-            <td>{{ student.strandName }}</td>
+            <td>{{ getLastWord(student.strandName) }}</td>
             <td>{{ student.age }}</td>
             <td>{{ student.gender }}</td>
             <td>{{ student.juniorHighSchool }}</td>
             <td>{{ student.jhsLocation }}</td>
             <td>{{ student.formattedJhsYear }}</td>
+            <td>grade</td>
             <td>
               <div
                 class="alert alert-danger mb-0"
@@ -98,9 +175,13 @@ export default {
     return {
       searchResult: [],
       filter: "",
-      allStudents: [],
       // selected filter
-      filterBy: "firstName"
+      filterBy: "strandName",
+      searchBy: "",
+      // pull up strand
+      selectedStrand: "",
+      selectedGrade: "",
+      strandList: []
     };
   },
   created() {
@@ -116,11 +197,81 @@ export default {
       .then(response => {
         this.searchResult = response.data.response;
         // this.searchResult = response.data;
-        console.log(response.data);
       })
       .catch(e => {
         console.log(e);
       });
+  },
+
+  methods: {
+    getLastWord(strandName) {
+      const last = strandName.split(" ").splice(-1);
+      // return last[last.length - 1];
+      return last[0].replace(/[()]/g, "");
+    },
+
+    searchStrand() {
+      const token = localStorage.getItem("token");
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json"
+        }
+      };
+      const param = this.selectedStrand;
+      axios
+        .get(
+          `http://localhost:3000/api/v1/admin/student/strand/${param}`,
+          config
+        )
+        .then(response => {
+          this.searchResult = response.data.response;
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+
+    searchGrade() {
+      const token = localStorage.getItem("token");
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json"
+        }
+      };
+      const param = this.selectedGrade;
+      axios
+        .get(
+          `http://localhost:3000/api/v1/admin/student/gradelevel/${param}`,
+          config
+        )
+        .then(response => {
+          this.searchResult = response.data.response;
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+
+    getAllStrand() {
+      const token = localStorage.getItem("token");
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json"
+        }
+      };
+      axios
+        .get("http://localhost:3000/api/v1/admin/strand", config)
+        .then(response => {
+          const loadStrand = response.data.strandData;
+          this.strandList = loadStrand;
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
   },
 
   computed: {
@@ -128,8 +279,7 @@ export default {
       const filterRes = this.filterBy;
       return this.searchResult.filter(item => {
         const value = item[filterRes];
-        const result = this.filter;
-        return value.match(result);
+        return value.match(this.filter);
       });
     }
   },
@@ -145,6 +295,9 @@ export default {
 </script>
 
 <style scoped>
+.container {
+  max-width: 1500px;
+}
 tbody {
   display: block;
   height: 600px;
