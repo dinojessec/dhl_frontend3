9a6ed5e05fef88727569df7a96df9066785e5438
Jesse Dino <jesse.dino.z3@gmail.com>
2019-04-28 08:51:48 +0800

    pending design

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="container">
+    <div class="row">
+      <div class="col">
+
+      </div>
+    </div>
 
     <router-link to="/admin/strand"><button
         type="button"
         class="btn btn-outline-primary btn-lg btn-block mb-2"
-      >Strand</button></router-link>
+      >Strand List</button></router-link>
     <router-link to="/admin/studentlist">
       <button
         type="button"
         class="btn btn-outline-primary btn-lg btn-block mb-2"
-      >Student</button></router-link>
+      >Student List</button></router-link>
     <button
       type="button"
       class="btn btn-outline-primary btn-lg btn-block mb-2"
