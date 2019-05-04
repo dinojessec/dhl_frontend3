6627dc8b503b593f0e83634db6c35f5be2f6e73b
Jesse Dino <jesse.dino.z3@gmail.com>
2019-05-04 09:52:45 +0800

    fixed home and about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,12 +1,18 @@
 <template>
-  <div>
-    <h1>this is about page</h1>
+  <div class="container">
+    <h4>Address</h4>
+    <p>Sta. Maria Compound, Marcos Hi-way, Santolan (3.85 mi)
+      Pasig 1610</p>
+    <h4>Phone</h4>
+    <p>(02) 682 4625</p>
+    <h4>Social Media</h4>
+    <p>www.facebook.com/Deecoacademy</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'about',
+  name: "about"
 };
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,12 +3,15 @@
     <carousel
       :minSwipeDistance="8"
       :per-page="1"
+      :autoplay="true"
+      :navigationClickTargetSize="8"
+      navigationPrevLabel
     >
       <slide>
-        <h1>Welcome to DHL</h1>
+        <h1>Welcome To Dee Hwa Liong Academy</h1>
       </slide>
       <slide>
-        <h1>Slide 2</h1>
+        <h1><img src="../assets/53111054_2000350213415013_3805721025782480896_n.jpg" /></h1>
       </slide>
     </carousel>
   </div>
